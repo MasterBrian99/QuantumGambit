4a52bd1da08f452ae1a99a98c42a5a6bcfb02711
Pasindu Pramodya <git@pasindupramodya.com>
2024-12-09 20:12:54 +0530

    initial commit gui

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="w-full container mx-auto">
+      <h1 className="text-3xl font-bold">Welcome to Tauri + React</h1>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+        <Button onClick={()=>{
+                    greet()
+        }} type="submit" variant={"destructive"}>Gasdreet</Button>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
